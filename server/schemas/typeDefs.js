8a7dcf8f45a1f193724fd6aa0f1f812864c875d9
Rachel Fritz <rachelfritz0@gmail.com>
2020-10-21 09:02:59 -0500

    setting up typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,7 +2,33 @@
 const { gql } = require('apollo-server-express');
 
 // create our typeDefs
-const typeDefs = gql``;
+const typeDefs = gql`
+
+type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+  }
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook: (book: BookInput): User
+    removeBook: (bookId: String!): User
+  }
+
+input BookInput {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
+`;
 
 // export the typeDefs
 module.exports = typeDefs;
