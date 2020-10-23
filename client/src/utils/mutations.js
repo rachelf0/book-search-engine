661c713b0dace6b3ec744372abeafd5f288f3fca
Rachel Fritz <rachelfritz0@gmail.com>
2020-10-23 10:35:55 -0500

    added to mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -24,8 +24,12 @@ export const ADD_USER = gql`
     addUser(username: $username, email: $email, password: $password) {
       token
       user {
-        _id
         username
+        email
+        bookCount
+        savedBooks {
+          title
+        }
       }
     }
   }
